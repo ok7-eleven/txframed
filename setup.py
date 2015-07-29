dab2b15088396808b822a7cfe46b8253be233830
Michael Mattice <mmattice@ok7-eleven.com>
2015-07-29 09:56:21 -0500

    add setup.py

---
+++ setup.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+setup(name='txframed',
+      version='0.9',
+      url='http://github.com/mmattice/txframed/',
+      description='Twisted Python Framed Protocol',
+      author='Michael Mattice',
+      author_email='mike@mattice.org',
+      packages=['txframed',],
+     )
